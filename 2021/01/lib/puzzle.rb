585b3208ad221fe9f83be7de1673a5a1227a6e71
Danial Pearce <danial.pearce@cultureamp.com>
2022-11-25 14:41:20 +1100

    golf it

--- 2021/01/lib/puzzle.rb
+++ 2021/01/lib/puzzle.rb
@@ -4,15 +4,7 @@
 class Puzzle
   class << self
     def call(input)
-      prev = input[0]
-      count = 0
-
-      input[1..].each do |item|
-        count += 1 if item > prev
-        prev = item
-      end
-
-      count
+      input[1..].each_with_index.map{ |x, i| [x, input[i]] }.select{|x| x[0] > x[1] }.size
     end
   end
 end
