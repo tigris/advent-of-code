6f9185a4076bc42b17340a712968f0f651fc614a
Danial Pearce <danial.pearce@gmail.com>
2022-11-28 21:27:58 +1100

    rubocop

--- 2021/06/lib/puzzle.rb
+++ 2021/06/lib/puzzle.rb
@@ -4,16 +4,16 @@
 class Puzzle
   class << self
     def call(input, days = 256)
-      age_counts = Hash.new(0).tap{|x| input.each{|i| x[i] += 1 } }
+      age_counts = Hash.new(0).tap { |x| input.each { |i| x[i] += 1 } }
 
       days.times do
         x = Hash.new(0).tap do |i|
-          age_counts.each do |k,v|
+          age_counts.each do |k, v|
             if k.zero?
               i[6] += v
               i[8] += v
             else
-              i[k-1] += v
+              i[k - 1] += v
             end
           end
         end
