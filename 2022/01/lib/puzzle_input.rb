eb3e99ac79a92e790382825fcc39fbcd02d63985
Danial Pearce <danial.pearce@cultureamp.com>
2022-12-01 17:33:51 +1100

    golf'd again now i read about .chunk() properly

--- 2022/01/lib/puzzle_input.rb
+++ 2022/01/lib/puzzle_input.rb
@@ -4,7 +4,7 @@
 class PuzzleInput
   class << self
     def call(file)
-      File.readlines(file).map(&:chomp).chunk(&:empty?).map { |_, x| x }.reject { |x| x == [''] }.map { |x| x.map(&:to_i) }
+      File.readlines(file).map(&:chomp).chunk(&:empty?).map { |x, y| x ? nil : y }.compact.map { |x| x.map(&:to_i) }
     end
   end
 end
