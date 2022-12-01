bbd74dd46993bb7b5d2ca4cc321c1cddb110227a
Danial Pearce <danial.pearce@cultureamp.com>
2022-12-01 17:42:36 +1100

    1 more golf

--- 2022/01/lib/puzzle_input.rb
+++ 2022/01/lib/puzzle_input.rb
@@ -4,7 +4,7 @@
 class PuzzleInput
   class << self
     def call(file)
-      File.readlines(file).map(&:chomp).chunk(&:empty?).map { |x, y| x ? nil : y }.compact.map { |x| x.map(&:to_i) }
+      File.readlines(file).map(&:chomp).chunk(&:empty?).map { |x, y| x ? nil : y.map(&:to_i) }.compact
     end
   end
 end
