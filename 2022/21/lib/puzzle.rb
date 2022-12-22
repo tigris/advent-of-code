f18457dee3abe9b5c378f3607f25a2a7595d19d9
Danial Pearce <danial.pearce@cultureamp.com>
2022-12-22 12:12:14 +1100

    Cleanup

--- 2022/21/lib/puzzle.rb
+++ 2022/21/lib/puzzle.rb
@@ -5,12 +5,12 @@ class Puzzle
   class << self
     def part1(input)
       define_methods(input)
-      new.root
+      root
     end
 
     def part2(input, increment = 100_000_000_000.to_f)
       input[:root][1] = :==
-      input[:humn] = 0.to_f
+      input[:humn] = increment
 
       define_methods(input)
 
@@ -18,11 +18,12 @@ class Puzzle
       old_diff = nil
 
       loop do
-        left = new.send(input[:root][0])
-        right = new.send(input[:root][2])
-        new_diff = (left - right).abs
+        left = send(input[:root][0])
+        right = send(input[:root][2])
         # p "#{input[:humn]} => #{left} == #{right}"
         break if left == right
+
+        new_diff = (left - right).abs
 
         if old_diff && new_diff > old_diff
           # We're getting worse, switch it
@@ -31,7 +32,6 @@ class Puzzle
 
         input[:humn] += increment
 
-        remove_method(:humn)
         define_methods(input.slice(:humn))
         old_diff = new_diff
       end
@@ -41,12 +41,15 @@ class Puzzle
 
     def define_methods(input)
       input.each do |k, v|
+        # rubocop:disable Security/Eval,Style/EvalWithLocation,Style/DocumentDynamicEvalDefinition
+        eval "undef :#{k}" if respond_to?(k)
+        # rubocop:enable Security/Eval,Style/EvalWithLocation,Style/DocumentDynamicEvalDefinition
         if v.is_a?(Array)
-          define_method(k) do
+          define_singleton_method(k) do
             [send(v[0]), send(v[2])].reduce(v[1])
           end
         else
-          define_method(k) do
+          define_singleton_method(k) do
             v
           end
         end
