2ecdf2f49850d12296ac6ccaf736a32db2d712b9
Danial Pearce <danial.pearce@cultureamp.com>
2024-12-02 13:29:16 +1100

    Adding 2024 day 1 in ruby for comparison

---
+++ 2024/01.ruby/spec/puzzle_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'puzzle'
+
+RSpec.describe Puzzle do
+  context 'with provided examples' do
+    let(:provided_input) do
+      [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+      ]
+    end
+
+    describe '.part1' do
+      let(:expected_answer) { 11 }
+
+      it 'returns the correct result' do
+        expect(described_class.part1(provided_input)).to eq(expected_answer)
+      end
+    end
+
+    describe '.part2' do
+      let(:expected_answer) { 31 }
+
+      it 'returns the correct result' do
+        expect(described_class.part2(provided_input)).to eq(expected_answer)
+      end
+    end
+  end
+end
